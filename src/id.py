fbdcd3610e98183a22b2b899c289e256db2e11be
Fresno <fresno@users.noreply.github.com>
2018-08-23 09:34:51 +0800

    fix coding bug
    classify japanese musics by changing file name

--- src/id.py
+++ src/id.py
@@ -3,42 +3,50 @@ import eyed3
 import os
 import shutil
 
-from eyed3.compat import unicode
-
-path = "G:\\Music\\Albums\\Mai Kuraki"  # Audiofiles' path
+path = "G:\Music"  # Audiofiles' path
 os.chdir(path)
 files = os.listdir(path)
 s = []
 for file in files:
     filepath = os.path.join(path, file)
     if os.path.isfile(filepath):  # ignore folders and other files
-        if os.path.splitext(filepath)[-1] == ".mp3":
+        if os.path.splitext(filepath)[-1].lower() == ".mp3":
             s.append(file)
         else:
             pass
 
 print(os.getcwd())
 for file in s:
-    filepath = os.path.join(path, file)
+    print("Current file:" + file)
+    os.rename(file, "temp.mp3")  # because eyed3 cannot load file path with japanese characters
+    filepath = os.path.join(path, "temp.mp3")
     eyed3.LOCAL_ENCODING = 'UTF-8'
     audiofile = eyed3.load(filepath)
     try:
         album = audiofile.tag.album
-
-        albumfolder = os.path.join(path, album)
-        if not album == "":
+        if album is not None and not album == "":
+            for character in ["\\", "/", ":", "*", "?", "\"", "<", ">", "|"]:
+                album = album.replace(character, " ")
+            album = str.strip(album.strip("."))
+            albumfolder = os.path.join(path, album)
             if os.path.exists(albumfolder) and os.path.isdir(albumfolder):
-                # shutil.move(file, album)
+                os.rename("temp.mp3", file)
+                shutil.move(file, album)
                 pass
             else:
                 try:
                     os.mkdir(str.replace(album, "/", " "))
                 except FileExistsError:
                     pass
-                # shutil.move(file, album)
+                os.rename("temp.mp3", file)
+                shutil.move(file, album)
+        else:
+            print("skip file " + file + " since there's no album")
+            os.rename("temp.mp3", file)
     except AttributeError:
         print("skip " + file)
         pass
+
 # audiofile.tag.artist = u"Integrity"
 # audiofile.tag.album = u"Humanity Is The Devil"
 # audiofile.tag.album_artist = u"Integrity"
